9e46d5cc6e82b83d1da01e823416eb4d9e27e19c
kang yang <kangyang1@lenovo.com>
2014-05-15 09:36:06 +0800

    update

--- app-shell-web/src/main/java/com/lenovo/vctl/apps/image/upload/controller/FileUploadController.java
+++ app-shell-web/src/main/java/com/lenovo/vctl/apps/image/upload/controller/FileUploadController.java
@@ -7,6 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -45,111 +47,141 @@ import com.lenovo.vctl.cloudstorage.model.RecordObject;
 
 @Controller
 public class FileUploadController extends BaseController {
-	private static Log log = LogFactory.getLog(FileUploadController.class);
+    private static Log log = LogFactory.getLog(FileUploadController.class);
 
-	private static String sPath = "/data/fileUploads";
-	private static File directory = null;
-	
-	String fileSizeMax="42106880";
-	String sizeMax="805242880";
-	private static int sizeThreshold = 1024;
-	
-	private FileUpload upload = null;
-	private DiskFileItemFactory factory = null;
-	
-	@PostConstruct
-	public void init() {
-		sPath=PropertiesUtil.getFileProperties("filestorage.properties", "FileRepository");
-		if (sPath != null && sPath.endsWith("/"))
-			sPath = sPath.substring(0, sPath.length() - 1);
-		if (StringUtils.isNotEmpty(sPath)) {
-			directory = new File(sPath);
-			try {
-				FileUtils.forceMkdir(directory);
-			} catch (IOException e) {
-				log.error("init error ", e);
-				directory = null;
-			}
-		}
-		
-		factory = new DiskFileItemFactory();
-		factory.setSizeThreshold(sizeThreshold);
-		if (directory != null) {
-			factory.setRepository(directory);
-		}
-		fileSizeMax =PropertiesUtil.getFileProperties("filestorage.properties", "FileSizeMax"); 
-		sizeMax = PropertiesUtil.getFileProperties("filestorage.properties", "SizeMax");
-		upload = new ServletFileUpload(factory);
-		upload.setSizeMax(NumberUtils.toLong(sizeMax, -1l));
-		upload.setFileSizeMax(NumberUtils.toLong(fileSizeMax, -1));
-		log.info("upload init complete.");
-	}
+    private static String sPath = "/data/fileUploads";
+    private static File directory = null;
 
-	@RequestMapping(value = "/file/uploadFile", method = RequestMethod.POST)
-	public void uploadFile(HttpServletRequest request, HttpServletResponse response,ModelMap model) throws Exception {
-		long start = System.currentTimeMillis(),localend=start,ftpend=start;
-		String url = null;
-		DiskFileItem file=null;
-		try {
-			List<DiskFileItem> fileItems = new ArrayList<DiskFileItem>();
-			Map<String, String> requestParamenters = new HashMap<String, String>();
-			fillFileItemAndRequestParameter(request, requestParamenters, fileItems);
-			
-			String now = DateUtil.format(new Date(), DateUtil.FORMAT_LONG_STRING_DATE);
-			String fileName = now + "-" + UUID.randomUUID().toString();
-			File f = new File(sPath + "/" + fileName + ".bak");
-			FileOutputStream fos = new FileOutputStream(f);
-			file = fileItems.get(0);
-			
-			InputStream is = file.getInputStream();
-			int n=1;
-			ByteArrayOutputStream[] baos = new ByteArrayOutputStream[n];
-			ByteArrayInputStream[] bais = new ByteArrayInputStream[n];
-			try {
-				for (int j = 0; j < n; j++) {
-					byte[] buff = new byte[100*1024];
-					int rc = 0;
-					baos[j] = new ByteArrayOutputStream();
-					while ((rc = is.read(buff)) > 0) {
-						fos.write(buff, 0, rc);
-						baos[j].write(buff, 0, rc);
-					}
-					bais[j] = new ByteArrayInputStream(baos[j].toByteArray());
-				}
-				// resin本地存一份
-				fos.flush();
-				localend = System.currentTimeMillis();
-				// 存ftp
-				String[] fs = new String[] { fileName };
-				List<RecordObject> list = Store.getInstance().createFiles(bais, fs, UploadType.File.getSuffix(), UploadType.File.getType(),1, 0, false);// 保存到云存储使用异步方式
-				ftpend = System.currentTimeMillis();
-				url = list.get(0).getFtpUrl();
-			} catch (Exception e) {
-				log.error(e.getMessage(), e);
-			} finally {
-				for (int j = 0; j < bais.length; j++) {
-					try {
-						bais[j].close();
-					} catch (Exception e) {
-						log.error(e.getMessage());
-					}
-				}
-				if (fos != null)
-					fos.close();
-			}
-			model.put("url", url);
-			model.put(CREATEAT, System.currentTimeMillis());
-			model.put(ERROR_CODE, ErrCode.NO_ERROR);// 没有错误
-		} catch (Exception e) {
-			model.put(ERROR_CODE, ErrCode.ERROR_UPLOAD_UNKNOWEN_ERROR);
-			model.put(ERROR_INFO, "server error");
-			log.error("doUploadAudio error :" + e.getMessage(), e);
-		}
-		log.info("upload file success file="+file.getName()+",url="+url);
-		log.info("upload file time,local="+(localend-start)+",ftp="+(ftpend-localend)+",total="+(ftpend-start)+",size="+file.getSize());
-		response.getWriter().write(new JSONObject(model).toString());
-	}
+    String fileSizeMax = "42106880";
+    String sizeMax = "805242880";
+    private static int sizeThreshold = 1024;
 
+    private FileUpload upload = null;
+    private DiskFileItemFactory factory = null;
+
+    @PostConstruct
+    public void init() {
+        sPath = PropertiesUtil.getFileProperties("filestorage.properties", "FileRepository");
+        if (sPath != null && sPath.endsWith("/"))
+            sPath = sPath.substring(0, sPath.length() - 1);
+        if (StringUtils.isNotEmpty(sPath)) {
+            directory = new File(sPath);
+            try {
+                FileUtils.forceMkdir(directory);
+            } catch (IOException e) {
+                log.error("init error ", e);
+                directory = null;
+            }
+        }
+
+        factory = new DiskFileItemFactory();
+        factory.setSizeThreshold(sizeThreshold);
+        if (directory != null) {
+            factory.setRepository(directory);
+        }
+        fileSizeMax = PropertiesUtil.getFileProperties("filestorage.properties", "FileSizeMax");
+        sizeMax = PropertiesUtil.getFileProperties("filestorage.properties", "SizeMax");
+        upload = new ServletFileUpload(factory);
+        upload.setSizeMax(NumberUtils.toLong(sizeMax, -1l));
+        upload.setFileSizeMax(NumberUtils.toLong(fileSizeMax, -1));
+        log.info("upload init complete.");
+    }
+
+    @RequestMapping(value = "/file/uploadFile", method = RequestMethod.POST)
+    public void uploadFile(HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
+        long start = System.currentTimeMillis(), localend = start, ftpend = start;
+        String url = null;
+        DiskFileItem file = null;
+        try {
+            List<DiskFileItem> fileItems = new ArrayList<DiskFileItem>();
+            Map<String, String> requestParamenters = new HashMap<String, String>();
+            fillFileItemAndRequestParameter(request, requestParamenters, fileItems);
+            String suffix = getSuffix(fileItems);
+            System.out.println("suffix is : " + suffix);
+            if (StringUtils.isEmpty(suffix)) {
+                suffix = ".jpeg";
+            }
+            String now = DateUtil.format(new Date(), DateUtil.FORMAT_LONG_STRING_DATE);
+            String fileName = now + "-" + UUID.randomUUID().toString();
+            File f = new File(sPath + "/" + fileName + "." + suffix);
+            FileOutputStream fos = new FileOutputStream(f);
+            file = fileItems.get(0);
+
+
+            InputStream is = file.getInputStream();
+            int n = 1;
+            ByteArrayOutputStream[] baos = new ByteArrayOutputStream[n];
+//			ByteArrayInputStream[] bais = new ByteArrayInputStream[n];
+            try {
+                for (int j = 0; j < n; j++) {
+                    byte[] buff = new byte[100 * 1024];
+                    int rc = 0;
+//					baos[j] = new ByteArrayOutputStream();
+                    while ((rc = is.read(buff)) > 0) {
+                        fos.write(buff, 0, rc);
+//						baos[j].write(buff, 0, rc);
+                    }
+//					bais[j] = new ByteArrayInputStream(baos[j].toByteArray());
+                }
+                // resin本地存一份
+                fos.flush();
+                localend = System.currentTimeMillis();
+                // 存ftp
+//				String[] fs = new String[] { fileName };
+//				List<RecordObject> list = Store.getInstance().createFiles(bais, fs, UploadType.File.getSuffix(), UploadType.File.getType(),1, 0, false);// 保存到云存储使用异步方式
+//				ftpend = System.currentTimeMillis();
+//				url = list.get(0).getFtpUrl();
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+                log.error(e.getMessage(), e);
+            } finally {
+//				for (int j = 0; j < bais.length; j++) {
+//					try {
+//						bais[j].close();
+//					} catch (Exception e) {
+//						log.error(e.getMessage());
+//					}
+//				}
+                if (fos != null)
+                    fos.close();
+            }
+//			model.put("url", url);
+            model.put(CREATEAT, System.currentTimeMillis());
+            model.put(ERROR_CODE, ErrCode.NO_ERROR);// 没有错误
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            model.put(ERROR_CODE, ErrCode.ERROR_UPLOAD_UNKNOWEN_ERROR);
+            model.put(ERROR_INFO, "server error");
+            System.out.println("doUploadAudio error :" + e.getMessage());
+        }
+//		log.info("upload file success file="+file.getName()+",url="+url);
+//		log.info("upload file time,local="+(localend-start)+",ftp="+(ftpend-localend)+",total="+(ftpend-start)+",size="+file.getSize());
+        response.getWriter().write(new JSONObject(model).toString());
+    }
+
+    private String getSuffix(List<DiskFileItem> fileItems) {
+        String suffix = "";
+        if (null != fileItems) {
+            DiskFileItem fileItem = fileItems.get(0);
+            if (null != fileItem) {
+                String fileName = null;
+                try {
+                    fileName = URLDecoder.decode(fileItem.getName(), "utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace(System.out);
+                }
+                System.out.println(fileName);
+                String[] strSub =fileName.split("\\.");
+                System.out.println("length : "+strSub.length);
+                for(String str: strSub){
+                    System.out.println(str);
+                }
+                suffix = strSub[strSub.length - 1];
+                System.out.println("suffix is : " + suffix);
+            }
+        }
+        return suffix;
+    }
 
     @Override
     public FileUpload getUpload() {
@@ -157,19 +189,21 @@ public class FileUploadController extends BaseController {
     }
 
     public void fillFileItemAndRequestParameter(HttpServletRequest req, Map<String, String> requestParamenters, List<DiskFileItem> fileItems)
-			throws FileUploadException, UnsupportedEncodingException {
-		List /* FileItem */items = upload.parseRequest(new ServletRequestContext(req));
-		if (CollectionUtils.isNotEmpty(items)) { // 有文件存在
-			for (Iterator iterator = items.iterator(); iterator.hasNext();) {
-				DiskFileItem fileItem = (DiskFileItem) iterator.next();
-				if (fileItem.isFormField()) {
-					String name = fileItem.getFieldName();
-					String value = fileItem.getString("UTF-8");
-					requestParamenters.put(name, value);
-				} else {
-					fileItems.add(fileItem);
-				}
-			}
-		}
-	}
+            throws FileUploadException, UnsupportedEncodingException {
+        List /* FileItem */items = upload.parseRequest(new ServletRequestContext(req));
+        if (CollectionUtils.isNotEmpty(items)) { // 有文件存在
+            for (Iterator iterator = items.iterator(); iterator.hasNext(); ) {
+                DiskFileItem fileItem = (DiskFileItem) iterator.next();
+                if (fileItem.isFormField()) {
+                    String name = fileItem.getFieldName();
+                    String value = fileItem.getString("UTF-8");
+                    System.out.println("name : " + name + " value : " + value);
+                    requestParamenters.put(name, value);
+                } else {
+                    fileItems.add(fileItem);
+                    System.out.println("fileNames: " + fileItem.getName());
+                }
+            }
+        }
+    }
 }
