91cb44988cd47f886dcddd81a3ce22e6575ecbaf
dizhenxiong <kangyang1@lenovo.com>
2014-05-16 06:42:55 +0800

    update

--- shell/app-shell-web/src/main/java/com/lenovo/vctl/apps/controller/PlayerController.java
+++ shell/app-shell-web/src/main/java/com/lenovo/vctl/apps/controller/PlayerController.java
@@ -3,7 +3,6 @@ package com.lenovo.vctl.apps.controller;
 import com.lenovo.vctl.apps.constants.DalConstants;
 import com.lenovo.vctl.apps.model.Material;
 import com.lenovo.vctl.apps.service.MasterialService;
-import com.sun.org.apache.bcel.internal.generic.DALOAD;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -49,12 +48,10 @@ public class PlayerController {
                         String password) throws Exception {
 
         if (DalConstants.NAME_CUSTOMER.equals(name) && DalConstants.PASSWD_CUSTOMER.equals(password)) {
-            return "redirect:/memory/list?id="+DalConstants.ID_CUSTOMER;
-        }
-        else if(DalConstants.NAME_SPEC.equals(name) && DalConstants.ID_SPCE.equals(password)){
-            return  "redirect:/memory/list?id="+DalConstants.ID_SPCE;
-        }
-        else {
+            return "redirect:/memory/list?id=" + DalConstants.ID_CUSTOMER;
+        } else if (DalConstants.NAME_SPEC.equals(name) && DalConstants.ID_SPCE.equals(password)) {
+            return "redirect:/memory/list?id=" + DalConstants.ID_SPCE;
+        } else {
             return "index";
         }
 
@@ -90,17 +87,33 @@ public class PlayerController {
     }
 
     /**
+     * 准备审核某个预案
+     */
+    @RequestMapping(value = "/memory/checkdetail")
+    public String checkdetail(HttpServletRequest request, HttpServletResponse response, Long id, ModelMap model) throws Exception {
+        Material material = masterialService.getEntity(id);
+        model.put("material", material);
+        return "file/check";
+    }
+
+    @RequestMapping(value = "/memory/check")
+    public String check(HttpServletRequest request, HttpServletResponse response, Long id, String comment,int status,ModelMap model) throws Exception {
+        return "redirect:/memory/list?id=" + DalConstants.ID_SPCE;
+    }
+
+    /**
      * 获取所有预案的列表
      */
     @RequestMapping(value = "/memory/list")
-    public String list(HttpServletRequest request, HttpServletResponse response,String id, ModelMap model) throws Exception {
+    public String list(HttpServletRequest request, HttpServletResponse response, String id, ModelMap model) throws Exception {
 
-        if(DalConstants.ID_CUSTOMER.equalsIgnoreCase(id)){
+        if (DalConstants.ID_CUSTOMER.equalsIgnoreCase(id)) {
             List<Material> materialList = masterialService.getAllMaterialList();
+            model.addAttribute("userId", id);
             model.addAttribute("materials", materialList);
-        }
-        else if(DalConstants.ID_SPCE.equalsIgnoreCase(id)){
+        } else if (DalConstants.ID_SPCE.equalsIgnoreCase(id)) {
             List<Material> materialList = masterialService.getNewMaterialList();
+            model.addAttribute("userId", id);
             model.addAttribute("materials", materialList);
         }
         return "file/list";
