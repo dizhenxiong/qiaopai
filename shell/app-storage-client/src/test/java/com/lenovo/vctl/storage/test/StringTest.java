d35271036419d4a5a6822e339f4536958a645d3a
xdylxdyl@163.com <xdylxdyl>
2014-05-15 18:32:37 +0800

    sss

--- shell/app-storage-client/src/test/java/com/lenovo/vctl/storage/test/StringTest.java
+++
@@ -1,8 +0,0 @@
-package com.lenovo.vctl.storage.test;
-
-public class StringTest {
-	public static void main(String[] args) {
-		String test = "http://p1.ifaceshow.com/a/2012/1010/1900/abcccde_12394_abc.jpg";
-		System.out.println(test.replaceAll("http://", "").replaceAll("\\.ifaceshow.com", ""));
-	}
-}
