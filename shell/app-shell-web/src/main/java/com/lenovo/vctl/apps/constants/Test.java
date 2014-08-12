41852c46fb380c31d93a163ed0cf95e621668a27
kang yang <kangyang1@lenovo.com>
2014-08-12 15:18:39 +0800

    update

---
+++ shell/app-shell-web/src/main/java/com/lenovo/vctl/apps/constants/Test.java
@@ -0,0 +1,18 @@
+package com.lenovo.vctl.apps.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kangyang1
+ * Date: 14-8-12
+ * Time: 下午2:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            int c = (int) (Math.random() * 1000);
+            System.out.println(c);
+        }
+
+    }
+}
